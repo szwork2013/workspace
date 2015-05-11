076acc8ac341412ba804a08435118afd38ddebfa
yuta-univ <yk.milab.kochiu@gmail.com>
2015-05-11 18:20:50 +0900

    knock update
    update

--- 100knock/knock8_9.py
+++ 100knock/knock8_9.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import random
 #com
 #0:暗号化
 #1:復号化
@@ -24,15 +24,34 @@ def cipher(message,encode):
 
 def q_1_8(message):
 	moji_list=cipher(message,0)
-	print moji_list
-	#moji_list3=cipher(moji_list2,1)
-	#print moji_list3
-	#for moji in moji_list:
-	#	cipher(moji)
+	text="".join(moji_list)
+	print text
+	moji_list2=cipher(text,1)
+	text="".join(moji_list2)
+	print text
+
 	return 1
 
 def q_1_9(message):
+	listWord=message.split(" ")
+	wordChanged=""
+	#print listWord
+	for word in listWord:
+		if len(word) > 4:
+
+			wordHead=word[:1]
+			wordTail=word[len(word)-1:len(word)]
+			wordBody=word[1:len(word)-1]
+			listWordBody=list(wordBody)
+			random.shuffle(listWordBody)
+			wordBody="".join(listWordBody)
+			print wordHead+wordBody+wordTail,
+		else:
+			print word,
+
 	return 1
 
+print "Q8."
 q_1_8("Let us never negotiate out of fear. But let us never fear to negotiate.")
+print "Q9."
 q_1_9("I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind .")
