2d3a5452610d5804bed99d1173bc9e587701d701
utau1116b <utao.utau.1116.yk@gmail.com>
2015-05-20 12:58:40 +0900

    knock update
    to 18

--- 100knock/knock2.py
+++ 100knock/knock2.py
@@ -4,6 +4,20 @@
 import codecs
 import json as json
  
+#各地域の最高気温入れるオブジェクト
+class HighTemp:
+	def __init__(self,name_prefecture,name_region,temp_high,date):
+		self.name_prefecture=name_prefecture
+		self.name_region=name_region
+		self.temp_high=temp_high
+		self.date=date
+
+#utf-8でエンコードして出力
+def print_encode8(string):
+	print string.encode("utf-8")
+
+
+ #リストや辞書の中身を表示する
 def pp(obj):
   if isinstance(obj, list) or isinstance(obj, dict):
     orig = json.dumps(obj, indent=4)
@@ -11,18 +25,22 @@ def pp(obj):
   else:
     print obj
 
+#ファイルを行単位d読み込んでリストとして返す
 def linereader_knock2(m_filename):
 	m_file_open=codecs.open(m_filename,"r","utf-8")
 	m_lines=m_file_open.readlines()
 	return m_lines
 
+#渡したファイルの行数を数えて表示
 def knock10(m_filename):
 	print sum(1 for line in open(m_filename))
 
+#タブをスペースに変換して表示する
 def knock11(m_filename):
 	for line in open(m_filename):
 		print line.replace('\t',' ')
 
+#各行の1列目をcol1.txtに、2列目をcol2.tx5に保存
 def knock12(m_filename):
 	col1=open("col1.txt","w")
 	col2=open("col2.txt","w")
@@ -33,6 +51,7 @@ def knock12(m_filename):
 	col1.close()
 	col2.close()
 
+#col1とcol2をマージしてタブ区切りで表示
 def knock13(m_filename):
 	m_col1=codecs.open("col1.txt","r","utf-8")
 	m_col2=codecs.open("col2.txt","r","utf-8")
@@ -52,7 +71,7 @@ def knock13(m_filename):
 	m_col2.close()
 	m_col3.close()
 
-	#m_lines=codecs.open(m_filename,"r","utf-8")
+#受け取ったファイルの先頭からN行を出力
 def knock14(m_filename,m_show_line_head):
 	m_count=0
 	for line in open(m_filename):
@@ -60,7 +79,7 @@ def knock14(m_filename,m_show_line_head):
 		m_count=m_count+1
 		if m_count >= m_show_line_head:
 			break
-
+#受け取ったファイルの末尾のN行を出力
 def knock15(m_filename,m_show_line_tail):
 	m_count=0
 	m_file_open=open(m_filename)
@@ -73,6 +92,7 @@ def knock15(m_filename,m_show_line_tail):
 		if m_count >= m_show_line_tail:
 			break
 
+#受け取ったファイルをN分割する
 def knock16(m_filename,m_num_split):
 	m_count=0
 	m_num_file=1
@@ -89,12 +109,25 @@ def knock16(m_filename,m_num_split):
 			m_file_knock16=open("file_knock16_"+str(m_num_file),"w")
 
 #http://d.hatena.ne.jp/t-fridge/20080323/1206217930
+#各業の1列目の異なる文字列の集合を求めて表示
 def knock17(m_filename):
 	m_lines=linereader_knock2(m_filename)
 	m_list_initial=[]
 	for line in m_lines:
 		m_list_initial.append(line[0:1].encode("utf-8"))
 	pp(list(set(m_list_initial)))
+
+def knock18(m_filename):
+	m_lines=linereader_knock2(m_filename)#m_linesに行ずつ読み込み
+	m_list_hightemp=[]#HighTempオブジェクトのリスト
+	for line in m_lines:
+		m_temp_high=line.encode("utf-8").split()
+		m_list_hightemp.append(HighTemp(m_temp_high[0].decode("utf-8"),m_temp_high[1].decode("utf-8"),m_temp_high[2].decode("utf-8"),m_temp_high[3].decode("utf-8")))
+
+	m_list_hightemp_sorted=sorted(m_list_hightemp, key=lambda hightemp: hightemp.temp_high,reverse=False)
+	for temp_high in m_list_hightemp_sorted:
+		print temp_high.name_prefecture.encode("utf-8")+" "+temp_high.name_region.encode("utf-8")+" "+temp_high.temp_high.encode("utf-8")+" "+temp_high.date.encode("utf-8")
+
 
 
 g_filename="hightemp.txt"
@@ -103,7 +136,7 @@ print "10"
 print "11"
 #knock11(filename)
 print "12"
-knock12(g_filename)
+#knock12(g_filename)
 print "13"
 #knock13(filename)
 print "14"
@@ -113,6 +146,7 @@ print "15"
 print "16"
 #knock16(g_filename,10)
 print "17"
-#knock17(g_filename)
+knock17(g_filename)
 print "18"
+knock18(g_filename)
 print "19"
---
+++ HTML/rokusenji/js/digi.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+function top_center_banner(){
+	document.write('<div id="pickup">');
+    document.write(' <ul class="clearfix">');
+    document.write(' <li><a href="dr.html" rel="nofollow"><img src="images/pickup-dr.jpg" width="170" height="102" alt="院長紹介" /></a></li>');
+    document.write('<li class="nil"><a href="contact.html" rel="nofollow"><img src="images/pickup-contact.jpg" width="170" height="102" alt="お問い合わせ" /></a></li>');
+    document.write('</ul>');
+    document.write('<p><a href="https://www.shika-town.com/reservation.php?pre=search&amp;cl=39004" rel="nofollow"><img src="images/big-ban-reserve.jpg" width="350" height="60" alt="WEBで24時間受付中　診療予約はこちら" /></a></p>');
+    document.write('</div>');
+}
+function right_banner(){
+	document.write('<div id="column_b">');
+          document.write('<h3 id="title03">六泉寺歯科 診療内容</h3>');
+          document.write('<dl>');
+            document.write('<dt><span class="raquo">&raquo;</span> 人口歯根・インプラントについて</dt>');
+              document.write('<dd><a href="implant.html"><img src="images/pickup-implant.jpg" width="180" height="70" alt="インプラントのページはこちらから" /></a></dd>');
+            document.write('<dt><span class="raquo">&raquo;</span> 口元の美を追求する 審美治療</dt>');
+              document.write('<dd><a href="aesthe.html"><img src="images/pickup-aesthe.jpg" width="180" height="70" alt="審美治療のページはこちらから" /></a></dd>');
+            document.write('<dt><span class="raquo">&raquo;</span> 歯列矯正治療について</dt>');
+              document.write('<dd><a href="ortho.html"><img src="images/pickup-ortho.jpg" width="180" height="70" alt="歯列矯正のページはこちらから" /></a></dd>');
+            document.write('<dt><span class="raquo">&raquo;</span> 虫歯・歯周病や小児向け治療</dt>');
+              document.write('<dd><a href="general.html"><img src="images/pickup-general.jpg" width="180" height="70" alt="虫歯・歯周病治療のページはこちらから" /></a></dd>');
+            document.write('<dt><span class="raquo">&raquo;</span> 健康的な口腔内環境を</dt>');
+              document.write('<dd><a href="prevent.html"><img src="images/pickup-prevent.jpg" width="180" height="70" alt="専門家のケア・PMTCについてはこちらから" /></a></dd>');
+          document.write('</dl>');
+        document.write('</div>');
+}
